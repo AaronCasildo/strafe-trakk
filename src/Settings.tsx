1c56b25ce10d6d5585ee4c562b9a8ee12c106cd5
AaronCasildo <aacasildor@gmail.com>
2026-02-15 21:01:45 -0800

    feat: add settings window contents

---
+++ src/Settings.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function Settings() {
+  return (
+    <div className="container">
+      <h1>Settings</h1>
+
+      <div style={{ marginTop: "30px" }}>
+        <h3>About Strafe Trakk</h3>
+        <p>Version 1.0.0</p>
+        <p>Track your keyboard timing for improved strafing performance.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Settings;
